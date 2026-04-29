3248ff4ed31ec3afa736c7a5b435b1824b61a1ca
kashish <kashishsalvi06@gmail.com>
2026-04-30 02:27:01 +0530

    Update CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const authRoutes = require('./routes/auth.routes');
 const musicRoutes = require('./routes/music.routes');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://spotifyclonemern.onrender.com'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
