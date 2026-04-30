3bed8f28094a04b21dc01822a9465bd2d42b5eca
kashish <kashishsalvi06@gmail.com>
2026-04-30 15:50:53 +0530

    aaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
